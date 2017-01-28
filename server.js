d54e135a3c0810065b8687c23cc9cf9c9fbc6f98
Cody Loyd <codyloyd@gmail.com>
2017-01-28 08:02:13 -0600

    push

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ MongoClient.connect(mongourl, (err, database) => {
 })
 
 
-app.get('/todo',(req,res) => {
+app.get('/',(req,res) => {
   db.collection('todo').find().toArray((err,results) =>{
     if (err) return console.log(err);
     return res.json(results)
@@ -32,22 +32,3 @@ app.post('/todo', (req,res) => {
   })
 })
 
-app.put('/quotes', (req, res) => {
-  db.collection('quotes').findOneAndUpdate(
-    {name: 'Yoda'},
-    {$set:{name:req.body.name,quote:req.body.quote}},
-    {sort:{_id: -1},upsert:true},
-    (err,result) => { 
-      if (err) return res.send(err); 
-      res.send(result)
-    }
-  )
-})
-
-app.delete('/quotes', (req, res) => {
-  db.collection('quotes').findOneAndDelete({name: req.body.name},
-  (err, result) => {
-    if (err) return res.send(500, err)
-    res.send('A darth vadar quote got deleted')
-  })
-})
